6fe5c33db178f30a1abb4b98b1a3b04e8888597b
Vinay-webDev <justchatting3000@gmail.com>
2024-12-07 00:15:23 +0530

    Update one.py

--- twopointers/one.py
+++ twopointers/one.py
@@ -71,4 +71,16 @@ array = [1, 1, 2, 3, 3, 4]
 array2 = [100, 100, 300, 400]
 class Solution:
     def remove_duplicates(self, arr):
-        
+        j = 1 #pointer for next position
+
+        for i in range(1, len(arr)):
+            if arr[i] != arr[i - 1]:
+                arr[j] = arr[i]
+                j += 1
+        return arr[:j]
+sol = Solution()
+print(sol.remove_duplicates(array)) #[1, 2, 3, 4]
+print(sol.remove_duplicates(array2)) #[100, 300, 400]
+
+
+
